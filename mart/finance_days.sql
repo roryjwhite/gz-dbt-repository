6d75821d822a9e520009837007d02325f01b2533
roryjwhite973@gmail.com <roryjwhite973@gmail.com>
2024-05-07 15:45:01 +0000

    finance

--- mart/finance_days.sql
+++ mart/finance_days.sql
@@ -1,9 +1,9 @@
 SELECT date_date
     , COUNT(orders_id) AS nb_transactions
-    , ROUND(SUM(total_revenue),2) AS revenue
-    , ROUND(SUM(total_revenue)/COUNT(orders_id),2) AS avg_basket
-    , ROUND(SUM(sum_margin),2) AS margin
-    , ROUND(SUM(operational_margin),2) AS operational_margin
+    , SUM(total_revenue) AS revenue
+    , SUM(total_revenue)/COUNT(orders_id) AS avg_basket
+    , SUM(sum_margin) AS margin
+    , SUM(operational_margin) AS operational_margin
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
 ORDER BY date_date DESC
