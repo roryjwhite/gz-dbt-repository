58b7c979c9998cff0f59695fc85371c4ba3cfcc1
roryjwhite <166918069+roryjwhite@users.noreply.github.com>
2024-05-08 13:05:44 +0100

    Revert "Marketing"

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,9 @@
+SELECT orders_id
+    , date_date
+    , ROUND(SUM(revenue),2) AS total_revenue
+    , ROUND(SUM(quantity),2) AS total_qty
+    , ROUND(SUM(purchase_cost),2) AS total_purchase_cost
+    , ROUND(SUM(margin),2) AS sum_margin
+FROM {{ref("int_sales_margin")}}
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC, orders_id DESC
