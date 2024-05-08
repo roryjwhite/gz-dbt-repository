7ca983500d32a104fa7aca55056cea5d3113641a
roryjwhite973@gmail.com <roryjwhite973@gmail.com>
2024-05-08 14:00:48 +0000

    schema updates

--- models/mart/finance_campaigns_day.sql
+++ models/mart/finance_campaigns_day.sql
@@ -1,5 +1,12 @@
-SELECT *
+SELECT date_date
     , ROUND(operational_margin - ads_cost,1) AS ads_margin
+    , avg_basket
+    , operational_margin
+    , revenue    
+    , nb_transactions
+    , ads_cost
+    , ads_impression
+    , ads_click    
 FROM {{ref("finance_days_mark")}}
 JOIN {{ref("int_campaigns_day")}}
 USING (date_date)
